a8eb1a969b2d8c65dad8ac0294c19c4cd6f6654d
heriwang <wangsharry812@gmail.com>
2026-01-27 08:35:48 +0700

    feat: implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers;
+class StudentController
+{
+    public function index()
+    {
+        echo '<h1>Daftar Siswa</h1>';
+        echo '<p>Menampilkan Daftar Siswa</p>';
+    }
+
+    public function create()
+    {
+        echo '<h1>Tambah Siswa</h1>';
+        echo '<p>Menampilkan Form Tambah Siswa</p>';
+    }
+}
+
+
+
